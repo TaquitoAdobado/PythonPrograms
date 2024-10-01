9065542f24d2877d1055c6cc21ad4944d411556f
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-01 01:06:54 -0600

    Se agrega mensaje de animo

--- Hola Mundo/Hola Mundo.py
+++ Hola Mundo/Hola Mundo.py
@@ -1,3 +1,3 @@
 print ("hola mundo")
 #Con print mostramos un mensaje el cual debe estar entre parentesis y si es un texto debe ponerse entre comillas ""
-#Branch Exercises
+"#De aqui en adelante todo es facil :)"
