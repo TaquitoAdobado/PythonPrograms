ccd6632b8d3cbbddb6fe061fb07c90c29ea22639
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-01 00:35:48 -0600

    Version 1.0 Ejercicios con if y while, entre otras cosas

--- Hola Mundo/Hola Mundo.py
+++ Hola Mundo/Hola Mundo.py
@@ -1,2 +1,2 @@
 print ("hola mundo")
-#Con print mostramos un mensaje el cual debe estar entre parentesis y si es un texto debe ponerse entre comillas
+#Con print mostramos un mensaje el cual debe estar entre parentesis y si es un texto debe ponerse entre comillas ""
