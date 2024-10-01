1d39dc9358c87abf601218a4cdd651e6f00dc9fa
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-01 00:15:27 -0600

    Se regresa todo como estaba

--- Hola Mundo/Hola Mundo.py
+++ Hola Mundo/Hola Mundo.py
@@ -1,3 +1,2 @@
 print ("hola mundo")
 #Con print mostramos un mensaje el cual debe estar entre parentesis y si es un texto debe ponerse entre comillas
-#Nueva Linea de prueba para nueva version en mi repositorio
