a2eba7aafeb0d288a5610d95eb1b1777a2945912
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-05 21:09:17 -0600

    Se agrega 'Copia de fraccion de una lista'

---
+++ Listas, Rebanadas/Copia de fraccion de una lista.py
@@ -0,0 +1,6 @@
+# Se puede usar [start:end] para copiar una fraccion de una lista de la siguiente manera
+#Donde star es la posicion desde donde se copiara y end sera la ultima posicion -1 a copiar.
+
+My_list1 = [1, 2, 3, 4, 5]  #Primero creamos una lista
+my_list2 = My_list1[1:4]    #Copiamos la lista desde la posicion 1 (el numero 2), hasta la posicion 4-1 (el numero 4)
+print(my_list2)     #El resultado debe ser del numero 2 hasta el numero 4
