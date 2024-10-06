bdb96dc37fc82921a7c075b611c09ea65d1b6a6f
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-05 19:54:44 -0600

    Se agrega Ordenamiento con .sort()

---
+++ Ordenamiento de Listas/Ordenamiento con herramienta sort().py
@@ -0,0 +1,3 @@
+my_list = [8, 6, 5, 4, 2]       #Creamos una lista
+my_list.sort()      #.sort() es una herramienta lista para usar de python que ordena listas de forma automatica.
+print(my_list) #el resultado debería ser: 2, 4, 5, 6, 8
