1ad137b80fe560dd8d9312f30ab14a8807d18d50
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-05 22:26:15 -0600

    Se actualiza mensaje de animo

--- Hola Mundo/Hola Mundo.py
+++ Hola Mundo/Hola Mundo.py
@@ -1,3 +1,3 @@
 print ("hola mundo")
 #Con print mostramos un mensaje el cual debe estar entre parentesis y si es un texto debe ponerse entre comillas ""
-"#De aqui en adelante todo es facil :)"
+print("De aqui en adelante todo es facil :) animo")
