f31678749833071c68b180b0e9fa09f5bf3e6f7b
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-07 19:59:52 -0600

    Se agrega Ejercicio 'Detector de palindromos'

---
+++ Detector de Palindromos/Detector de Palindromos.py
@@ -0,0 +1,8 @@
+print("Identificador de palindromos")   #Titulo del programa mostrado al usuario
+frase = input("ingrese su palabra o frase: ", )#Se ingresa una palabra o frase por el usuario
+frase_nueva = "".join(frase.split()).lower()  #Se eliminan todos los espacios entre las palabras si es que tienen.
+palindromo = frase_nueva==frase_nueva[::-1]  #Se crea un flag True o False si frase_sin espacio = frase_sin_espacio alrevez
+if palindromo:      #Si palindromo es True
+    print(f"La palabra o frase '{frase}' \nSI es un palindromo") #Si es True se imprime esta primera linea
+else:               #Si palindromo es Fale
+    print(f"La palabra o frase '{frase}' \nNO es un palindromo")  #Si es False se imprime esta segunda linea
