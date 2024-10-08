35277e79df3a6b4be3d64bfe45b6eaa6e1f6a6d2
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-08 00:03:02 -0600

    Se complementa programa 'Funcion con doble argumento' agregando argumentos posicionales y por palabras clave

--- Funciones/Funcion con doble argumento.py
+++ Funciones/Funcion con doble argumento.py
@@ -1,12 +1,25 @@
+#FUNCIONES DE 2 ARGUMENTOS
+
 def mensaje(cantidad, palabra):       #Se agregan 2 argumentos: cantidad y palabra
     print("ingresa", cantidad, palabra)   #Cuerpo de la funcion
 
+#------------------------------------------------------------------------------
+#Argumento posicional.
+#------------------------------------------------------------------------------
 
-mensaje(5, "chetos")   #Se invoca la funcion y se declaran los 2 argumentos
+mensaje(5, "chetos")   #Se invoca la funcion y se declaran los 2 argumentos por su posición en la función
 # Salida = Ingresa 5 chetos
 
-mensaje(2, "monedas")
-# Salida = ingresa 2 monedas
+#------------------------------------------------------------------------------
+#Argumento por palabra clave. cuando no importa el orden mientras declaremos los argumentos por su nombre
+#------------------------------------------------------------------------------
+
+mensaje(palabra="cervezas", cantidad=15)    #No importa el orden mientras declaremos los argumentos por su nombre
+# Salida = ingresa 15 cervezas
+
+#------------------------------------------------------------------------------
+# Recordemos que tambien se pueden declarar los argumentos antes de invocar a la funcion
+#------------------------------------------------------------------------------
 
 cantidad = int(input("ingresa una cantidad: "))
 palabra = input("ingrese de que: ")
