526ab2040b1f5ac762e72a14ab76357b659afa5b
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-07 20:10:06 -0600

    Se corrige comentario en linea 4

--- Detector de Palindromos/Detector de Palindromos.py
+++ Detector de Palindromos/Detector de Palindromos.py
@@ -1,7 +1,7 @@
 print("Identificador de palindromos")   #Titulo del programa mostrado al usuario
 frase = input("ingrese su palabra o frase: ", )#Se ingresa una palabra o frase por el usuario
-frase_nueva = "".join(frase.split()).lower()  #Se divide frase en palabras, se juntan sin espacio y se convierten a minucula
-palindromo = frase_nueva==frase_nueva[::-1]  #Se crea un flag True o False si frase_sin espacio = frase_sin_espacio alrevez
+frase_nueva = "".join(frase.split()).lower()  #Se divide frase en palabras, se juntan sin espacio y se convierten a minuscula
+palindromo = frase_nueva==frase_nueva[::-1]  #Se crea un flag True o False si frase_nueva = frase_nueva alrevez
 if palindromo:      #Si palindromo es True
     print(f"La palabra o frase '{frase}' \nSI es un palindromo") #Si es True se imprime esta primera linea
 else:               #Si palindromo es False
