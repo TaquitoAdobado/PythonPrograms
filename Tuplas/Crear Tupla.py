8e1823fde0da97dc24e1debef6aadd60569b3844
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-16 01:09:17 -0600

    Se crea carpeta 'Tuplas' en las que se explicará que son y como funcionan. Se agrega programa 'Crear Tupla'

---
+++ Tuplas/Crear Tupla.py
@@ -0,0 +1,26 @@
+#Una tupla es una secuencia inmutable (No puede editarse)
+#Cada elemento en una tupla puede ser de distinto tipo, como int, float y string
+
+#Se crea igual que una lista pero usando () en lugar de []
+
+tuple_1 = (1,2,4,8)
+
+# o simplemente separando los valores con ,
+
+tuple_2 = 1., .5, .25, "hi"
+
+#Una tupla puede crearse vacia
+
+tuple_3 =()
+
+#Se puede crear una tupla de un solo elemento, pero se debe agregar una "," al final para distinguirse de un valor ordinario
+
+tuple_4 = (5,) #Si quitamos la , tuple_4 = 5, no será una tupla, sera un valor asignado a una variable.
+#-------------------------------------------------------------------------------
+print(tuple_1)
+print(tuple_2)
+print(tuple_3)
+print(tuple_4)
+#-------------------------------------------------------------------------------
+
+
