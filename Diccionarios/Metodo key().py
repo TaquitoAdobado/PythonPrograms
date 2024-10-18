ba43ac60501a6fc6aa7bf0ddc20c80d2553d7d6d
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-18 01:33:02 -0600

    Se agrega programa 'Metodo key()' el cual enseña como al usar ese metodo creamos una lista con todas las key de un diccionario

---
+++ Diccionarios/Metodo key().py
@@ -0,0 +1,9 @@
+#El metodo .keys() retorna una LISTA con todas las KEY de un diccionario
+
+dictionary = {"monday":"lunes", "tuesday": "martes", "wednesday":"miercoles", "thursday":"jueves", "friday":"viernes", "saturday":"sabado", "sunday":"domingo"}
+print(dictionary.keys())
+#Salida: dict_keys(['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
+
+#Al tener la lista con todas las claves, se puede acceder a todo el diccionario
+for i in dictionary.keys():
+    print(i, "->", dictionary[i])
