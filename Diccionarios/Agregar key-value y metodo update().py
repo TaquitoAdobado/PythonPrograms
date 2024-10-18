3a21ae1fee336dda5c68d1691b9115d744ba0df3
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-18 02:07:21 -0600

    Se añade programa 'Agregar key-value y metodo update()' en el que se enseñan 2 maneras de agregar pares de key:value a un diccionario

---
+++ Diccionarios/Agregar key-value y metodo update().py
@@ -0,0 +1,19 @@
+#Para agregar una key:value, se tiene que asignar un valor a una key que no exista, como si quisieras modificarlo.
+
+dictionary = {"happy":"feliz", "sad":"triste", "angry":"enojado"}
+print (dictionary)
+#Salida: {'happy': 'feliz', 'sad': 'triste', 'angry': 'enojado'}
+
+dictionary["bored"]= "aburrido"     #Como "bored" no se encuentra en el diccionario, se agregará con su value "aburrido"
+print(dictionary)
+#Salida: {'happy': 'feliz', 'sad': 'triste', 'angry': 'enojado', 'bored': 'aburrido'}
+
+#Otra manera de agregar una key:value se usa el metodo .update({key:value})
+
+dictionary.update({"anxious":"ansioso"})
+print(dictionary)
+#Salida: 
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+#Para eliminar una key de un diccionario
