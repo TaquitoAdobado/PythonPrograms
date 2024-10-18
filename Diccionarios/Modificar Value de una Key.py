00c78e514e8c373f3f557119ca0bc347b2f9c8c6
TaquitoAdobado <terandaniel94@outlook.com>
2024-10-18 01:52:00 -0600

    Se agrega programa 'Modificar Value de una Key' en el que se explica el proceso para editar el value de una key

---
+++ Diccionarios/Modificar Value de una Key.py
@@ -0,0 +1,9 @@
+#Para modificar el valor de una key, es muy parecido a modificar una lista, solo que en lugar de buscar la posicion, buscamos la key.
+
+dictionary = {"one":"uno", "two":"dos", "three":"tres"}
+print (dictionary)
+
+dictionary["one"]= 1
+dictionary["two"]= 2
+dictionary["three"]= 3
+print(dictionary)
