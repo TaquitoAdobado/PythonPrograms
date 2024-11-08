b094fb9be12ab7da33669feb0003626ba826e5aa
TaquitoAdobado <terandaniel94@outlook.com>
2024-11-07 22:12:59 -0600

    Se añade funcion para guardar y cargar lista de tareas gracias a un archivo JSON que se crea. Tambien se añaden comentarios al codigo, quedando pendiente de comentar la funcion de marcar y la de borrar, pero creo que ya no será necesario puesto que es el mismo proceso que tiene el resto de funciones.

--- Proyectos/TaskMaster.py
+++ Proyectos/TaskMaster.py
@@ -1,6 +1,22 @@
-import os   #Se importa el OS  para poder limpiar la pantalla. Cuando se llame usa 'cls' si el os.name =="nt" para windows o 'clear' para otro so (Linux/MAC)
+import json #Importamos el modulo json para poder hacer una copia de seguridad en un archivo de texto tipo json
+import os   #Importamos modulo os para poder limpiar la pantalla. Cuando se llame usa 'cls' si el os.name =="nt" para windows o 'clear' para otro so (Linux/MAC)
+#--------------------------------------------------------------------------------------------------------------------------------------------------------
+#Esta funcion guardrá las tareas registradas en un archivo JSON
+def Save_Tasks(task_list, filename="TaskMaster List.json"):  #Se define la funcion con sus argumentos, file name contiene el nombre default del archivo json que se creará
+    with open(filename, "w") as file:#Se abre el archivo filename en modo escritura (w), with asegura que el archivo se cierre despues de usarse, file es una referencia al archivo abierto.
+        json.dump(task_list, file) #Esta linea convierte la task_list en formato json y la escribe en el archivo file referenciado en la linea anterior.
 
-  #--------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------------------------
+#Esta funcion cargará las tareas desde un archivo JSON
+def Load_Tasks(filename="TaskMaster List.json"):#Se define la funcion y se usa de argumento el filename con el nombre del archivo que cargaremos.
+    try:       #Ejecutamos un try, mientras no haya un error en su bloque, no pasaremos al except.
+        with open(filename, 'r') as file:   #abrimos el archivo filename name en modo lectura (r), with asegura que el archivo se cierre despues de usarse, file es una referencia al archivo abierto. 
+            return json.load(file)  #En esta linea se lee el contenido de file y lo convierte de JSON a una lista de Python usando json.load.
+                                    #Si el archivo se lee correctamente, finaliza la función.
+    except FileNotFoundError:   #FileNotFoundError ocurre si el archivo filename no existe.
+        return []   #Si filename no existe, la funcion devuelve una lista vacia '[]', de tal forma que el programa funcione sin interrumpirse.
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------
 #La funcion main_menu crea la interfaz en la que se muestra la bienvenida al programa y un menú con diferentes opciones las cuales serán las de la lista menu_list
 # 1: View Tasks 
 # 2: Add Task 
@@ -47,6 +63,7 @@ def Add_Task(task_list):
             task_list.append(new_task)                                #Se agrega la nueva tarea al final de la lista
             View_Tasks(task_list)                                     #Se limpia pantalla y mostramos las tareas actuales
             print("\nThe task:", new_task + "\nhas been added")       #Mostramos mensaje de que la tarea se ha añadido
+            Save_Tasks(task_list)                                     #Guardamos la tasklist en un archivo json
             while True:                                               #Si el usuario añadió una tarea nueva, iniciamos un bucle nuevo:
                     add_new=input("\nDo you want to add another task? (y/n): ").lower() #Se le pregunta al usuario si quiere agregar algo mas (se guarda la respuesta en minuscula)
                     if add_new=="n":                                    #Si la respuesta es 'n'
@@ -62,39 +79,36 @@ def Add_Task(task_list):
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 #Esta función realizará la accion de la opción 3: Edit Task
 def Edit_Task(task_list):
-    View_Tasks(task_list)
-    while True:
+    View_Tasks(task_list)           #Cuando llamamos a la funcion editar, limpiamos la pantalla y mostramos la task list actual
+    while True:                     #Iniciamos un bucle
         try:
-            ed_task=int(input("\nWich task do you want to edit? \ninsert number or insert '0' to cancel: "))
-            if ed_task == 0:
-                os.system("cls" if os.name=="nt" else "clear")
-                break
-            elif 1<=ed_task<=len(task_list):
-                print("\nEditing task","'"+task_list[ed_task -1]+"'", "to: ", end="")
-                task_list[ed_task-1]=input("")
-                View_Tasks(task_list)
-                print("\nThe task has been edited successfully")
-                while True:
-                    try:
-                        edit_more=input("\nDo you want to edit another task? (y/n): ").lower()
-                        if edit_more=="n":
-                            os.system("cls" if os.name=="nt" else "clear")
-                            return
-                        elif edit_more=="y":
-                            View_Tasks(task_list)
-                            break
-                        else:
-                            View_Tasks(task_list)
-                            print("\nInvalid input, please try again...")
-                    except ValueError:
-                        View_Tasks(task_list)
-                        print("\nInvalid input, please try again...")
-            else:
-                View_Tasks(task_list)
-                print("\nTask", ed_task, "doesn't exist, please try again...")
-        except ValueError:
-            View_Tasks(task_list)
-            print("\nInvalid input, please try again...")
+            ed_task=int(input("\nWich task do you want to edit? \ninsert number or insert '0' to cancel: "))    #Se pregunta que tarea se quiere editar y se almacena valor en una variable
+            if ed_task == 0:                                        #Si el valor ingresado es 0:
+                os.system("cls" if os.name=="nt" else "clear")      #Limpiamos pantalla
+                return                                              #Regresamos al menu inicial
+            elif 1<=ed_task<=len(task_list):                               #Si se ingresa un numero tarea dentro del rango de tareas que existe:
+                print("\nEditing task","'"+task_list[ed_task -1]+"'", "to: ", end="")   #Mostramos un mensaje de que editaremos una trea, usamos ed_task -1 ya que hacemos alucion a la posicion en la lista
+                task_list[ed_task-1]=input("")                                          #Se sobreescribirá la tarea elegida por lo que ingrese el usuario
+                View_Tasks(task_list)                                                   #Limpiamos pantalla y mostramos lista de tareas actuales
+                Save_Tasks(task_list)                                                   #Guardamos la tasklist en un archivo json
+                print("\nThe task has been edited successfully")                        #Mostramos mensaje de tarea editada exitosamente (mero adorno)
+                while True:                                                             #Iniciamos nuevo bucle:
+                        edit_more=input("\nDo you want to edit another task? (y/n): ").lower()  #Se pregunta si quiere editar otra tarea
+                        if edit_more=="n":                                                      #Si se ingresa 'n'
+                            os.system("cls" if os.name=="nt" else "clear")                      #Limpiamos pantalla
+                            return                                                              #Regresamos al menu inicial
+                        elif edit_more=="y":                                                #Si se ingresa 'y':
+                            View_Tasks(task_list)                                           #Limpiamos pantalla, mostramos lista de tareas actuales
+                            break                                                           #Salimos de este bucle y regresamos al bucle anterior
+                        else:                                                           #Si se ingresa algo diferente a 'n' o 'y':
+                            View_Tasks(task_list)                                       #Limpuamos pantalla, mostramos lista de tareas actuales
+                            print("\nInvalid input, please try again...")               #Se muestra mensaje de entrada invalida y se repite el bucle
+            else:                                                           #Si se ingresa un numero de tarea fuera del rango de tareas que existe:
+                View_Tasks(task_list)                                       #Se limpia pantalla y muestran tareas actuales
+                print("\nTask", ed_task, "doesn't exist, please try again...")#Se muestra mensaje avisando que la tarea no existe y se repite el bucle
+        except ValueError:                                                   #Si se ingresa una entrada de otro tipo (como str)
+            View_Tasks(task_list)                                            #Se limpia pantalla y muestran tareas actuales
+            print("\nInvalid input, please try again...")                    #Se muestra mensaje de entrada invalida y se repite el bucle
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 def Mark_Task(task_list):
@@ -115,6 +129,7 @@ def Mark_Task(task_list):
                     task=task_list[marked_task-1]
                     task_list[marked_task-1]= "✔ "+ task
                     View_Tasks(task_list)
+                    Save_Tasks(task_list)
                     print("\nThe task has been marked")
                     while True:
                             mark_again=input("\nDo you want to mark another task? (y/n): ").lower()
@@ -127,15 +142,14 @@ def Mark_Task(task_list):
                             else:
                                 View_Tasks(task_list)
                                 print("\nInvalid input please try again...")
-                                continue
                 else:
                     while True:
                             unmark=input("\nTask already marked, do you want to unmark? (y/n): ").lower()
                             if unmark=="y":
                                 print(task_list[marked_task-1])
                                 task_list[marked_task-1]=task_list[marked_task-1].replace("✔ ","")
-                                print(task_list[marked_task-1])
                                 View_Tasks(task_list)
+                                Save_Tasks(task_list)
                                 print("\nThe task has been unmarked successfully")
                                 break
                             elif unmark=="n":
@@ -167,6 +181,7 @@ def Delete_Task(task_list):
                 elif 1<=del_task<=len(task_list):
                     del task_list[del_task-1]
                     View_Tasks(task_list)
+                    Save_Tasks(task_list)
                     print("\nThe task been deleted successfully") 
                     while True:
                         del_more=input("\nDo you want to delete another task? (y/n): ").lower()
@@ -191,37 +206,43 @@ def Delete_Task(task_list):
                 print("\nInvalid input, please try again...")
                 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
-
-#La función main será donde se ejecutará todo el programa
+#La función main() será donde se ejecutará todo el programa.
 def main(menu_list, task_list):
-    os.system("cls" if os.name=="nt" else "clear")
-    while True:
-        main_menu(menu_list)
+    os.system("cls" if os.name=="nt" else "clear")      #Al iniciar el programa limpiamos patalla
+    while True:                                         #Iniciamos un bucle
+        main_menu(menu_list)                            #Mostramos el menu de inicio
         try:
-            user_choice=int(input("\nPlease insert the number option you want or insert 0 to see menu again or: "))
-            if 0<=user_choice<=5:
-                mapping_menu[user_choice](task_list)
-            elif user_choice==6:
-                break
-            else:
-                print("\nThe option", user_choice, "doesn't exist. Please try again...")
-        except ValueError:
-            print("\nInvalid answer please try again...")
-    print("\nClosing program, until next time :D/")
-    return
+            user_choice=int(input("\nPlease insert the number option you want: ")) #Se pide al usuario ingresar una opcion del menu
+            if 1<=user_choice<=5:                   #El menu tiene 5 opciones, si se elige una entre 1-5:
+                option_menu[user_choice](task_list) #Usando la key 1-5 llamamos al value, que será una funcion, escribimos el argumento (task_list) que usara la funcion.
+            elif user_choice==6:                         #Si el usuario ingresa 6:
+                Save_Tasks(task_list)
+                break                                    #Salimos del bucle por lo que salimos del programa y se imprime mensaje de despedida
+            else:                                                                      #Si se escribe un numero fuera del rango 1-5:
+                print("\nThe option", user_choice, "doesn't exist. Please try again...")#Avisamos que esa opcion no existe
+        except ValueError:                                          #Si se ingresa una entrada con tipo diferente (como str)
+            print("\nInvalid answer please try again...")           #Avisamos de entrada invalida y repetimos el bucle
+    print("\nClosing program, until next time :D/")     #Mensaje de despedida
+    return                                              #Salimos del programa
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
-#Se crea la lista menu_list, al final de cada opcion tiene los espacios necesarios con su "|" para cuadrar con la interfaz.
-menu_list = ["1: View Tasks         |", "2: Add Task           |", "3: Edit Task          |", "4: Mark Task          |", "5: Delete Task        |", "6: Close Program      |"]
+#Al iniciar el programa se empieza por:
+if __name__ == "__main__" :#asegura que el código dentro de este bloque solo se ejecute cuando el archivo se ejecuta directamente.
+    #Se crea la lista menu_list, al final de cada opcion tiene los espacios necesarios con su "|" para cuadrar con la interfaz.
+    menu_list = ["1: View Tasks         |", "2: Add Task           |", "3: Edit Task          |", "4: Mark Task          |", "5: Delete Task        |", "6: Close Program      |"]
+    #Se crea la lista task_list. En esta lista se almacenarán todas las tareas registradas por el usuario.
+    task_list=Load_Tasks() #Cargamos el archivo JSON con tareas guardadas anteriormente
+    if not task_list:
+        task_list=[]    
 
-task_list=[]    #Se crea la lista task_list. En esta lista se almacenarán todas las tareas registradas por el usuario.
+    #Se crea el diccionario con las funciones para el menu
+    option_menu = {     #El diccionario contendra las funciones del programa que se llamaran a eleccion del usuario
+        1: View_Tasks,  # Se escriben las funciones sin () ni parametros, ya que esos se programan al llamarse en el codigo
+        2: Add_Task,
+        3: Edit_Task,
+        4: Mark_Task,
+        5: Delete_Task
+        }
 
-mapping_menu = {    #mapeo entre las opciones de la lista 'menu_list' y las funciones de cada opción seleccionada
-    1: View_Tasks,
-    2: Add_Task,
-    3: Edit_Task,
-    4: Mark_Task,
-    5: Delete_Task
-    }
-
-main(menu_list, task_list)  #Llamamos a la funcion main, la cual contiene el proceso del programa.
-
+    Load_Tasks  
+    #Se ejecuta la funcion main donde entraremos en bucle hasta que el usuario decida salir del programa.
+    main(menu_list, task_list)  #La cual contiene el proceso del programa.
