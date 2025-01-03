39e3ec39d2f6dd38bfae4333e13d876439c82e72
TaquitoAdobado <terandaniel94@outlook.com>
2025-01-02 19:34:14 -0600

    Se añade linea de codigo para ajustar el tamaño minimo de la ventana

--- Tkinter_GUI_Dev/my first tk application.py
+++ Tkinter_GUI_Dev/my first tk application.py
@@ -3,6 +3,7 @@ import tkinter as tk    #Se importa la libreria Tkinter y se le asigna el alias 
 root =tk.Tk()   #Se crea la ventana principal (main) de la aplicacion y se le asigna a la variable "root"
 root.title("My first Tkinter application")  #Se define el titulo de la ventana main
 
+root.minsize(400,400) #Se define el tamaño minimo de la ventana main en pixeles
 # En este espacio entre el titulo y el mainloop se escribirá el codigo de los widgets que se mostraran en la ventana main
 
 root.mainloop() #Inicia un loop infinito en el main que mantendrá actualizada la aplicacion por cualquier evento ocurrido en ella.
