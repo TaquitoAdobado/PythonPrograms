e95620761d8b7c78607e717017f031d9f2519e5b
TaquitoAdobado <terandaniel94@outlook.com>
2025-01-12 19:11:43 -0600

    Se añade 'Argumentos indefinidos.py', en el que se explica como se usan los *args en una funcion.

---
+++ Funciones/Argumentos indefinidos.py
@@ -0,0 +1,11 @@
+'''Usados en casos donde no se conoce de antemano el numero exacto de argumentos que pasarán en una funcion.
+Se usa la sintaxis *args para referirse a los argumentos adicionales.
+El nombre *args no es mandatorio, sin embargo, es sugerido como buena practica.
+Cualquier nombre iniciado en * referirá a esros argumentos de cantidad variable'''
+
+def sumatoria_argumentos(*args):
+    sumatoria=sum(args) #Para usarlo se llama sin el *
+    print(sumatoria)
+
+sumatoria_argumentos(1,2,3,4,5)# *args iterará por todos los argumentos indefinidos dados al invocar la funcion
+    #Salida = 15
