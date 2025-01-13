25b1b5e60084443bd3206204c961bf50eb7bc8d0
TaquitoAdobado <terandaniel94@outlook.com>
2025-01-12 22:12:30 -0600

    Se añade otro metodo para invocar los *args

--- Funciones/Argumentos indefinidos args.py
+++ Funciones/Argumentos indefinidos args.py
@@ -9,3 +9,8 @@ def sumatoria_argumentos(*args):
 
 sumatoria_argumentos(1,2,3,4,5)# *args iterará por todos los argumentos indefinidos dados al invocar la funcion
     #Salida = 15
+
+'''Los *args tambien pueden ser desempaquetados de una lista'''
+args = [20,30,50]
+sumatoria_argumentos(*args)#Al invocarse se hace uso del *
+#Salida = 100
