d77f3de101af4c4d3bfb02f8c97ed74fcb9a8941
TaquitoAdobado <terandaniel94@outlook.com>
2025-01-13 01:44:17 -0600

    Se optimiza codigo: El argumento indefinido *args toma todos sus valores y los convierte en una tupla, por lo que convertir la tupla en lista sobraba en el codigo. La variable lista se sustituye por 'args', siendo este la tupla dada por *args. Se cambia orden de numero y primer_valor en el if, por estetica. Se cambia invocacion de la funcion y se añade una invocacion extra, de tal forma podemos observar los resultados True y False a la vez.

--- Ejercicios/Ejercicio, Detector de 0 consecutivos.py
+++ Ejercicios/Ejercicio, Detector de 0 consecutivos.py
@@ -2,14 +2,14 @@ def detector_cero_consecutivo(*args):
     '''Esta funcion deberá poder recibir una cantidad indefinida de numeros y devolverá
     True si contiene el numero 0 repetido 2 veces consecutivas, de lo contrario devolverá
     False'''
-    lista=list(args)
-    primer_valor=lista[0]
+    primer_valor=args[0]
 
-    for numero in lista[1:]:
-        if numero==0 and primer_valor==0:
+    for numero in args[1:]:
+        if primer_valor==0 and numero==0:
             return True
         else:
             primer_valor=numero
             pass
     return False
-print(detector_cero_consecutivo(0,1,0,1,0,3,8,0))
+print(detector_cero_consecutivo(5,6,1,0,0,9,3,5)) #True
+print(detector_cero_consecutivo(6,0,5,1,0,3,0,1)) #False
