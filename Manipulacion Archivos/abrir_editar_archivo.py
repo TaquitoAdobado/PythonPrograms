6948036e480b59b3ba808612757e3a4a9a04e733
TaquitoAdobado <terandaniel94@outlook.com>
2025-01-22 00:39:30 -0600

    Se crea script 'abrir_editar_archivo.py' en el cual se explica los diferentes modos de apertura de un archivo (r,w,a), asi como tambien se explica como agregar contenido al archivo. Con este codigo se creó el archivo 'prueba2.txt', no es necesario para hacer funcionar el script, ya que el mismo codigo lo creará si no se tiene.

---
+++ Manipulacion Archivos/abrir_editar_archivo.py
@@ -0,0 +1,23 @@
+'''Cuando abrimos un archivo con open(), si no se especifica el modo de apetura, por defecto será
+en modo lectura (r).'''
+mi_archivo = open('prueba.txt','r') # == mi_archivo = open('prueba.txt')
+'''Cuando un archivo se abre en modo lectura, no se puede editar.'''
+mi_archivo.close()
+
+'''Para poder editar un archivo es necesario abrirlo en modo escritura
+Hay 2 formas de abrir un archivo en modo escritura, la primera es usando (w)
+La primera forma es abrirlo con (w)
+Si el archivo no existe SE CREARÁ.
+Si el archivo si existe SE ELIMINARÁ TODO SU CONTENIDO para poder editarlo'''
+
+mi_archivo_2 = open('prueba2.txt','w') #Se crea archivo prueba2.txt
+''' Para editar el archivo usaremos el metodo '.write()  '''
+mi_archivo_2.write("Hola Mundo")
+mi_archivo_2.close()
+
+''' La segunda forma es abrirlo con (a)
+Al usar (a), el contenido del archivo permanecerá intacto y el cursor se posicionará al final del archivo,
+despues del ultimo caracter. De esta forma si agregamos algo al archivo, se agregará despues del contenido existente.    '''
+
+mi_archivo_2 = open('prueba2.txt','a')
+mi_archivo_2.write("\nEstoy debajo de Hola Mundo")
