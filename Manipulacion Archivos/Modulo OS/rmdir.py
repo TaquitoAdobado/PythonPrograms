9609c0cd75284b0afcfc7444ef1ece19f758470d
TaquitoAdobado <terandaniel94@outlook.com>
2025-01-23 20:50:55 -0600

    Se crea script 'rmdir.py' en el que se explica como usar este metodo del modulo os para eliminar diccionarios

---
+++ Manipulacion Archivos/Modulo OS/rmdir.py
@@ -0,0 +1,7 @@
+import os
+
+''' os.rmdir (Remove Directory)
+Este metodo funciona para eliminar una carpeta indicando su ruta
+para este ejemplo creé la carpeta borrar la cual será eliminada cuando se ejecute la siguiente linea '''
+
+os.rmdir('C:\\Users\\danie\\Documents\\ProgramasPython\\Manipulacion Archivos\\Modulo OS\\borrar')
