40c9f1c985f855518a8c1aa22c441d7c7426cdd6
TaquitoAdobado <terandaniel94@outlook.com>
2025-01-28 19:56:55 -0600

    Se crea carpeta Modulo Path. Se añade script 'abrir_archivo_diferente_OS.py' en el cual se explica como usar el objeto Path para poder abrir archivos con una sintaxis en su ruta, de diferente OS. En otras palabras, compatibilidad.

---
+++ Manipulacion Archivos/Modulo Path/abrir_archivo_diferente_OS.py
@@ -0,0 +1,18 @@
+''' Usamos el objeto Path para poder reconocer la sintaxis de las rutas de otros sistemas operativos.'''
+from pathlib import Path
+
+#En Path agregamos una ruta con '/' el cual es usado en OS Mac y Linux
+carpeta = Path('C:/Users/danie/Documents/ProgramasPython/Manipulacion Archivos/Modulo OS/Carpeta')
+
+'''
+El siguiente paso se puede omitir si el archivo se agrega directamente en la ruta almacenada en la variable 'carpeta':
+    'C:/Users/danie/Documents/ProgramasPython/Manipulacion Archivos/Modulo OS/Carpeta/archivo.txt'
+o si se agrega fuera de path() en la misma variable 'carpeta':
+    carpeta = Path('C:/Users/danie/Documents/ProgramasPython/Manipulacion Archivos/Modulo OS/Carpeta') / 'archivo.txt'
+'''
+#Se indica el archivo que queremos usando la ruta almacenada en carpeta y añadiendo '/' + nombre del archivo
+archivo = carpeta / 'archivo.txt'
+
+#Probamos que el archivo se pueda abrir en nuestro OS Windows, aunque tenga la sintaxis de Mac/Linux
+mi_archivo = open(archivo)
+print(mi_archivo.read())
