168e3ec4a18b83af26a6848c0ab88d78d0bc4b37
TaquitoAdobado <terandaniel94@outlook.com>
2025-01-31 23:35:56 -0600

    Se añade al script una manera de ver el nombre de las carpetas haciendo uso del metodo .glob().

--- Manipulacion Archivos/Modulo Path/metodo_glob.py
+++ Manipulacion Archivos/Modulo Path/metodo_glob.py
@@ -1,15 +1,27 @@
 from pathlib import Path
-carpeta = Path('C:/Users/danie/Documents/ProgramasPython/Manipulacion Archivos/Modulo OS/Carpeta')
+ruta = Path('C:/Users/danie/Documents/ProgramasPython/Manipulacion Archivos/Modulo OS/Carpeta')
 
 #El metodo '.glob()' nos permite buscar archivos con un patron de busqueda
 #El patron de busqueda se usa con la sintaxis de la siguiente manera:
 
 # .glob('*.txt') para buscar archivos en una carpeta de una ruta especifica.
 print("\n .glob('*.txt')")
-for archivo in carpeta.glob('*.txt'):
-   print(archivo)
+for archivo in ruta.glob('*.txt'):
+   print(archivo.name)  #Si no se pone el metodo '.name' se imprime la ruta completa
 
 # .glob('**/*.txt') Para buscar archivos en todas las carpetas a partir de una ruta especifica.
 print("\n .glob('**/*.txt')")
-for archivos in carpeta.glob('**/*.txt'):
-    print(archivos)
+for archivos in ruta.glob('**/*.txt'):
+    print(archivos.name)
+
+# Para buscar las carpetas a partir de una ruta especifica.
+print("\n .glob('*') usando .is_dir()")
+for carpeta in ruta.glob('*'):
+    if carpeta.is_dir():    #Si es un directorio
+        print(carpeta.name)
+
+# Para buscar las carpetas y subcarpetas a partir de una ruta especifica.
+print("\n .glob('**/*') usando .is_dir()")
+for carpeta in ruta.glob('**/*'):
+    if carpeta.is_dir():    #Si es un directorio
+        print(carpeta.name)
