a41f3e3a7e99a6ee0e688f8a64b4ec9ad6e18d27
TaquitoAdobado <terandaniel94@outlook.com>
2025-07-07 20:41:14 -0600

    Se hacen cambios en la estructura del programa: *Se elimina la libreria keyboard, ya no se activa AVi con una combinacion de teclas, ahora al no estar siempre en un bucle esperando la activacion, se ahorran recursos. *Se corrije bug durante la activacion de busquedas en internet que agregaba "b" o "bu" en la busqueda. *Los dialogos de AVi ya no inician con "Hmm"
    Se añade una pequeña GUI usando Tkinter, con las siguientes funciones:
    *Boton de modo nocturno
    *Boton para hablar con AVi

--- Proyectos/AsistenteVoz/AsistenteVoz.py
+++ Proyectos/AsistenteVoz/AsistenteVoz.py
@@ -1,3 +1,4 @@
+import tkinter as tk            #Para crear una Interfaz Gráfica de Usuario.
 import speech_recognition as sr # Online
 import pocketsphinx             # Offline 
 """
@@ -9,16 +10,16 @@ Sin embargo, la opción offline solo reconoce idioma inglés.
 import pyttsx3              #Para convertir texto a voz
 import pywhatkit            #Para darle acceso a YouTube, Google, enviar mensajes por WA, etc.
 import pyjokes              #Para contar chistes
-import keyboard             #Para detectar si se presiona una tecla
 import webbrowser           #Para abrir paginas web
 import datetime             #Para obtener la fecha y hora actual
 #import wikipedia           #Para buscar en Wikipedia
 import time                 #Para medir tiempos
 from random import choice   #Para elegir un elemento aleatorio de una lista
 
-#Escuchar nuestro microfono y devolver el audio como texto
+
 
 def audio_a_texto_online():
+    ''' Esta funcion escucha nuestro microfono y devolver el audio como texto'''
 
     #Almacenar el reconocimiento de voz en una variable
     recognizer = sr.Recognizer()
@@ -69,7 +70,7 @@ def texto_a_voz(texto):
     engine = pyttsx3.init()
 
     # Configuracion de la voz (opcional)
-    engine.setProperty('rate', 150)  # Velocidad de la voz, 150 = velocidad normal
+    engine.setProperty('rate', 145)  # Velocidad de la voz, 150 = velocidad normal
     engine.setProperty('volume', 1)  # Volumen de la voz (0.0 a 1.0)
 
     # Reproducir el texto
@@ -113,7 +114,7 @@ def pedir_dia():
     }
     
     # Informar al usuario el dia de la semana actual
-    texto_a_voz(f"hmm, Hoy es {dias_semana[dia_semana]} {dia_actual} de {nombre_mes[mes_actual]} del {año_actual}") # Decir el dia de la semana actual
+    texto_a_voz(f"Hoy es {dias_semana[dia_semana]} {dia_actual} de {nombre_mes[mes_actual]} del {año_actual}") # Decir el dia de la semana actual
 
 
 def pedir_hora():
@@ -127,7 +128,7 @@ def pedir_hora():
     minutos = hora_actual.minute
 
     # Informar al usuario la hora actual
-    texto_a_voz(f"hmm, Ahora son las {hora} horas con {minutos} minutos")
+    texto_a_voz(f"Ahora son las {hora} horas con {minutos} minutos")
 
 
 def saludo_inicial():
@@ -141,22 +142,22 @@ def saludo_inicial():
 
     # Saludo por la mañana
     if 6 <= hora < 12:
-        texto_a_voz("hmm, ¡Buenos dias!, mi nombre es avi, si necesitas algo presiona alt más á más uvé.")
+        texto_a_voz("¡Buenos dias!, mi nombre es avi, si necesitas algo presiona el botón rojo nuevamente.")
 
     # Saludo por la tarde
     elif 12 <= hora < 20:
-        texto_a_voz("hmm, ¡Buenas tardes!, mi nombre es avi, si necesitas algo presiona alt más á más uvé.")
+        texto_a_voz("¡Buenas tardes!, mi nombre es avi, si necesitas algo presiona el botón rojo nuevamente.")
     
     # Saludo por la noche
     else:
-        texto_a_voz("hmm, ¡Buenas noches!, mi nombre es avi, si necesitas algo presiona alt más á más uvé.")
+        texto_a_voz("¡Buenas noches!, mi nombre es avi, si necesitas algo presiona el botón rojo nuevamente.")
 
 
 def pedir_chiste():
     ''' Esta funcion pide un chiste al usuario. '''
    
     # Obtener un chiste aleatorio
-    chiste = pyjokes.get_joke(language= 'es', category= 'all')  # Obtener un chiste en español
+    chiste = pyjokes.get_joke(language= 'es', category= "neutral")  # Obtener un chiste en español
     
     # Contar el chiste al usuario
     texto_a_voz(chiste)
@@ -166,31 +167,27 @@ def buscar_en_internet(peticion):
     ''' Esta funcion permite buscar en internet lo que el usuario pida. '''
     
     # Eliminar "busca" de la peticion
-    if "usca" in peticion:
-        busqueda = peticion.replace("usca", "")  # Eliminar la palabra "busca" de la peticion
-    elif "busca" in peticion:
+    if "busca" in peticion:
         busqueda = peticion.replace("busca", "")
-    elif "sca" in peticion:
-        busqueda = peticion.replace("sca", "")
 
     # Informar al usuario que se esta buscando
-    texto_a_voz("hmmm, enseguida lo busco.")
+    texto_a_voz("Enseguida lo busco.")
 
     # Usar pywhatkit para buscar en Google (abre el navegador con el resultado)
     pywhatkit.search(busqueda)
     # Informar al usuario que se ha completado la busqueda
-    texto_a_voz(f"hmmm, abriendo resultado en navegador.")
+    texto_a_voz(f"Abriendo resultado en navegador.")
 
 
 def responder_insulto():
     ''' Esta funcion responde a los insultos del usuario. '''
 
     respuestas = [
-    "hmmm, talvez sea eso, pero seré mas educada que tu. Que tengas bonito día.",
-    "hmmm, almenos a mí si me enseñaron a no ser grosera. Disfruta tu día.",
-    "hmmm, si así de sucia tienes la boquita, no me quiero ni imaginar tu... ejem. Hasta pronto.",
-    "hmmm, está mal enojarse de esa manera, ¿qué dirían tus padres o abuelos si te escucharan?. Piénsalo.",
-    "hmmm, yo no soy la persona que se está peleando con un asistente de voz, jajaja Nos vemos."
+    "Talvez sea eso, pero seré mas educada que tu. Que tengas bonito día.",
+    "Almenos a mí si me enseñaron a no ser grosera. Disfruta tu día.",
+    "Si así de sucia tienes la boquita, no me quiero ni imaginar tu... ejem. Hasta pronto.",
+    "Está mal enojarse de esa manera, ¿qué dirían tus padres o abuelos si te escucharan?. Piénsalo.",
+    "Yo no soy la persona que se está peleando con un asistente de voz, jajaja Nos vemos."
     ]
     # Elegir una respuesta aleatoria de la lista de respuestas
     respuesta = choice(respuestas)
@@ -199,11 +196,11 @@ def responder_insulto():
     texto_a_voz(respuesta)
     
 
-def abrir_pagina_web():
+def abrir_ESO_cloud():
     ''' Esta funcion abre una pagina web en el navegador. '''
     
     # Informar al usuario que se abrirá una pagina web
-    texto_a_voz("hmmm, abriendo the elder scroll online.")
+    texto_a_voz("Abriendo the elder scroll online.")
 
     # Abrir la pagina web en el navegador
     webbrowser.open("https://www.xbox.com/es-MX/play/launch/the-elder-scrolls-online/BRKX5CRMRTC2")  
@@ -211,125 +208,205 @@ def abrir_pagina_web():
 
 def centro_de_control():
     ''' Esta funcion es el centro de control del asistente de voz. Depende de la peticion del usuario, será la accion a realizar. '''
-    
+    boton_avi.config(state="active")  # Deshabilitar el boton mientras se procesa la peticion
+
     # Escuchar al usuario y convertir su peticion a minusculas
     pedido = audio_a_texto_online().lower()  # Escuchar al usuario y convertir su peticion a minusculas
 
+    try:
+        #Seleccion de peticiones
 
-    #Seleccion de peticiones
-
-    # Si el usuario pide la hora
-    if any(palabra in pedido for palabra in [
-        "qué hora es",
-        "dime la hora",
-        "hora actual",
-        "a qué hora estamos"
+        # Si el usuario pide la hora
+        if any(palabra in pedido for palabra in [
+            "qué hora es",
+            "dime la hora",
+            "hora actual",
+            "a qué hora estamos"
+            ]):
+            pedir_hora()
+            print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            return
+        
+        # Si el usuario pide el dia de la semana
+        elif any(palabra in pedido for palabra in [
+            "qué día es",
+            "dime el día",
+            "día de la semana",
+            "qué día de la semana es",
+            "qué día es hoy",
+            ]):
+            pedir_dia()
+            print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            return
+        
+        #Si el usuario saluda al asistente de voz
+        elif any(palabra in pedido for palabra in [
+            "salúdame",
+            "hola",
+            "buenos días",
+            "buenas tardes",
+            "buenas noches"
         ]):
-        pedir_hora()
-        print("\nEsperando 'alt + a + v' para reconocer peticion.")
-        return
-    
-    # Si el usuario pide el dia de la semana
-    elif any(palabra in pedido for palabra in [
-        "qué día es",
-        "dime el día",
-        "día de la semana",
-        "qué día de la semana es",
-        "qué día es hoy",
+            texto_a_voz("hmm, Hola, espero que estés teniendo un día precioso")
+            print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            return
+        
+        #Si el usuario pide un chiste
+        elif any(palabra in pedido for palabra in [
+            "cuéntame un chiste",
+            "dime un chiste",
+            "quiero escuchar un chiste",
+            "hazme reír",
+            "cuenta un chiste",
+            "cuéntame otro chiste",
+            "dime otro chiste"
         ]):
-        pedir_dia()
-        print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            pedir_chiste()
+            print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            return
+        
+        #Si el usuario pide que busque en internet
+        elif any(palabra in pedido for palabra in [
+            "busca",
+        ]):
+            buscar_en_internet(pedido)
+            print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            return
+        
+        #Si el usuario le dice una groseria al asistente.
+        elif any(palabra in pedido for palabra in [
+            "pendeja",
+            "puta",
+            "estúpida",
+            "maricona",
+            "perra",
+            "babosa",
+            "imbésil",
+            "imbécil",
+            "tonta"
+        ]):
+            responder_insulto()
+            print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            return
+        
+        elif pedido == "quiero jugar eso":
+            abrir_ESO_cloud()
+            return
+        
+        elif pedido == "silencio o no se comprendio el audio":
+            #Si el usuario no dice nada
+            texto_a_voz("Lo siento, no escuché nada. Vuelve a llamarme cuando me necesites.")
+            print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            return
+        
+        else:
+            #Si se pide algo que no se tenga contemplado
+            texto_a_voz("Lo siento, no puedo ayudarte con eso. Por favor, intenta llamarme de nuevo.")
+            print("\nEsperando 'alt + a + v' para reconocer peticion.")
+            return
+        
+    finally:
+        #Rehabilitar el boton del asistente de voz
+        boton_avi.config(state="normal")
         return
-    
-    #Si el usuario saluda al asistente de voz
-    elif any(palabra in pedido for palabra in [
-        "salúdame",
-        "hola"
-    ]):
-        texto_a_voz("hmm, Hola, espero que estés teniendo un día precioso")
-        print("\nEsperando 'alt + a + v' para reconocer peticion.")
-        return
-    
-    #Si el usuario pide un chiste
-    elif any(palabra in pedido for palabra in [
-        "cuéntame un chiste",
-        "dime un chiste",
-        "quiero escuchar un chiste",
-        "hazme reir",
-        "cuenta un chiste"
-    ]):
-        pedir_chiste()
-        print("\nEsperando 'alt + a + v' para reconocer peticion.")
-        return
-    
-    #Si el usuario pide que busque en internet
-    elif any(palabra in pedido for palabra in [
-        "busca",
-        "usca",
-        "sca"
-    ]):
-        buscar_en_internet(pedido)
-        print("\nEsperando 'alt + a + v' para reconocer peticion.")
-        return
-    
-    #Si el usuario le dice una groseria al asistente.
-    elif any(palabra in pedido for palabra in [
-        "pendeja",
-        "puta",
-        "estúpida",
-        "maricona",
-        "perra",
-        "babosa",
-        "imbésil",
-        "imbécil",
-        "tonta"
-    ]):
-        responder_insulto()
-        print("\nEsperando 'alt + a + v' para reconocer peticion.")
-        return
-    
-    elif pedido == "quiero jugar eso":
-        abrir_pagina_web()
-        return
-    
-    elif pedido == "silencio o no se comprendio el audio":
-        #Si el usuario no dice nada
-        texto_a_voz("Lo siento, no escuché nada. Vuelve a llamarme cuando me necesites.")
-        print("\nEsperando 'alt + a + v' para reconocer peticion.")
-        return
-    
-    else:
-        #Si se pide algo que no se tenga contemplado
-        texto_a_voz("Lo siento, no puedo ayudarte con eso. Por favor, intenta llamarme de nuevo.")
-        print("\nEsperando 'alt + a + v' para reconocer peticion.")
-        return
-
 
 #-------------------Programa principal--------------------
 
-# Saludo inicial al usuario
-saludo_inicial() 
-print("\nEsperando 'alt + a + v' para reconocer peticion.")
+# Crear la ventana principal de la aplicación
 
-# Bucle principal del asistente de voz
-while True:
-    #Esperar a que el usuario presione ctrl + shift + a para activar el asistente
-    if keyboard.is_pressed('alt + a + v'):
-        print("\nAsistente de voz activado. Presiona 'alt + esc' para cerrar el asistente.")
+root = tk.Tk()
 
-        #Informar al usuario que se ha activado el asistente.
-        texto_a_voz("hmm, me llamaste, ¿en qué puedo ayudarte?")
+# Configurar la ventana principal
 
-        #Recibir la peticion del usuario y comparala con el centro de control
-        centro_de_control()
+root.title("AVi - Asistente Virtual") # Título de la ventana
+tamaño_x = 450  # Ancho de la ventana
+tamaño_y = 200  # Alto de la ventana
+root.geometry("450x200")  # Tamaño de la ventana (ancho x alto)
+root.maxsize(tamaño_x, tamaño_y)  # Tamaño máximo de la ventana
+root.resizable(False, False)  # Permitir redimensionar la ventana
+
+#colores (codigos hexadecimales)
+
+blanco = "#FFFFFF"
+negro = "#101010"
+amarillo_luna = "#D4AC0D"
+naranja_sol = "#F39C12"
+rojo = "#E74C3C"
+verde = "#2EEC71"
+
+root.configure(bg=blanco)  # Color de fondo
+
+def modo_dia_noche():
+    ''' Cambia el color de fondo de la ventana al hacer clic en el botón. '''
+
+    if root.cget("bg") == blanco:
+        root.configure(bg=negro)
+        boton_modo_nocturno.configure(
+            text="☀",
+            bg = negro,
+            fg = naranja_sol,
+            activebackground= blanco,
+            activeforeground= naranja_sol
+            )
+        
+    elif root.cget("bg") == negro:
+        root.configure(bg=blanco)
+        boton_modo_nocturno.configure(
+            text="🌙",
+            bg = blanco,
+            fg = amarillo_luna,
+            activebackground= negro,
+            activeforeground= amarillo_luna
+            )
     
-    # Cerrar el programa
-    elif keyboard.is_pressed('alt + esc'):
-        texto_a_voz("Hasta luego, que tengas un buen día.")
-        print("\nAsistente de voz cerrado.")
-        break
+#Boton para alternar entre modo dia y modo noche
+boton_modo_nocturno = tk.Button(
+    root, 
+    text="🌙",
+    justify= "center",
+    command= modo_dia_noche, #Funcion asignada al boton
+    cursor = "hand2",        #Para cambiar el cursor al estar sobre el boton
+    bg = blanco,             #Color de fondo
+    fg = amarillo_luna,              #Color de fuente
+    relief="ridge",
+    activebackground= negro,
+    activeforeground= amarillo_luna,
+    width= 5,
+    borderwidth= 2     
+    )
+boton_modo_nocturno.pack()
+boton_modo_nocturno.place(x=399, y=0)
 
-    else:
-        # Si no se presiona ninguna tecla, esperar un poco
-        time.sleep(0.1)
-        continue
+#Boton para activar al asistente de voz
+boton_avi = tk.Button(
+    root,
+    width= 10,
+    height=2,
+    bg=rojo,
+    text="Hablar",
+    font = ("TKDefaultFont", 12),
+    justify="center",
+    cursor="hand2",
+    relief="raised",
+    borderwidth= 4,
+    activebackground = verde,
+    state = "normal",
+    command = centro_de_control
+
+)
+boton_avi.pack()
+boton_avi.place(x=180, y=40)
+
+#Cuadro de dialogo de AVI (AUN SIN IMPLEMENTAR)
+texto_avi = tk.Label(
+    state = "disabled",
+    bg = blanco,
+    border= 2,
+    text = "hola mundo", #
+    wraplength=445,
+    justify="center"
+)
+#texto_avi.pack()
+#texto_avi.place(x=5, y=120)
+
+root.mainloop() # Para mantener la ventana abierta
