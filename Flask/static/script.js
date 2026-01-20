6bd783347e746f23dad13d3dcf9d9358ac992d4e
Daniel Teran Guerra <terandaniel94@gmail.com>
2026-01-20 00:48:20 -0600

    Se agrega carpeta 'static'. Parte de la estructura minima para levantar un servidor Flask, esta carpeta debe contener recursos estaticos como hojas de estilo CSS, archivos Javacript, imagenes, fuentes, etc. En este caso incluye una hoja de estilo css llamada style.css la cual da formato y apariencia visual a un template.html, tambien se incluye un archivo script.js el cual dará funcionamiento e interaccion a este template (este archivo de momento esta vacio)

---
+++ Flask/static/script.js
@@ -0,0 +1,10 @@
+/* En este archivo.js se agregan los scripts JavaScript que se usaran en los templates.html para darle funcionalidad
+a los elementos de la pagina web, ejemplo:
+- botones
+- formularios
+- menus desplegables
+- imagenes
+- textos
+- animaciones
+- eventos del teclado o raton
+*/
