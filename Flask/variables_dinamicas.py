f638db5bbbf797e035b78cc8a4729bdead4cec04
Daniel Teran Guerra <terandaniel94@gmail.com>
2026-01-24 22:01:11 -0600

    Se agregan archivos variables_dinamicas.py e info_usuario.html. variables_dinamicas.py es un servidor flask en el que se explica (en su script) el uso de variables dinamicas en la URL y en los templates. Muestra un saludo personalizado al nombre usado en la URL. Tambien muestra la informacion de usuarios registrados accediendo a su URL dinamica. info_usuario.html es la estructura y contenido que tendra la url dinamica que muestra los datos de usuarios registrados.

---
+++ Flask/variables_dinamicas.py
@@ -0,0 +1,94 @@
+#Una variable dinamica es aquella que puede cambiar su valor dependiendo de la entrada del usuario.
+#Para poder usar variables dinamicas se utilizan los simbolos < >
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+#Definimos una ruta dinamica.
+#El valor que se use en <nombre> sera capturado y usado como parametro en la funcion.
+@app.route('/saludo/<nombre>')
+def saludo_nombre(nombre):
+    ''' Muestra un saludo con el nombre proporcionado en la URL.
+
+        Args:
+            nombre (str): Nombre capturado en la ruta dinamica.
+
+        Returns:
+            str: Saludo personalizado.
+
+        Example:
+            URL: /saludo/Daniel
+            Resultado: Bienvenido Daniel
+    '''
+    return f'<h1>Bienvenido {nombre}</h1>'
+
+
+#Con un diccionario de usuarios creados (datos_usuarios) mostraremos la informacion de un usuario renderizando una plantilla HTML.
+@app.route('/info/<usuario>')
+def info_usuario(usuario):
+    '''
+    Muestra la informacion de un usuario proporcionando su nombre en la URL.
+
+    Args:
+        usuario (str): Nombre de usuario capturado en la URL.
+
+    Returns:
+        str: Informacion del usuario renderizada en una plantilla HTML.
+
+    Usuarios registrados:
+        - TaquitoAdobado
+        - UsuarioFlask
+        - ProgramadorPython
+
+    Example:
+        URL: /info/TaquitoAdobado
+        Resultado:
+            Nombre: Taquito
+            Apellido: Adobado
+            Edad: 31
+            Sexo: Masculino
+    '''
+    usuario_crudo = usuario # Guardamos el nombre de usuario tal cual fue capturado para usarlo en mensajes de error.
+    usuario = usuario.lower() # Convertimos el nombre de usuario a minusculas para hacer la busqueda normalizada.
+    # Obtenemos los datos del nombre de usuario capturado en la URL.
+    datos = datos_usuarios.get(usuario)
+    #Si el usuario existe, renderizamos la plantilla con sus datos.
+    if datos:
+        return render_template('info_usuario.html', usuario = datos)
+        #key=usuario: nombre del usuario capturado en la URL.
+        #usuario=datos: diccionario con los datos del usuario.
+        
+    #Si el usuario no existe, mostramos un mensaje de error.
+    else:
+        return f'<h1><span style="color:red">ERROR:</span>Usuario {usuario_crudo} no registrado.</h1>'
+    
+    #NOTA: Asegurarse de tener la plantilla info_usuario.html en la carpeta templates.
+    
+
+
+#Diccionario con datos de usuarios.
+datos_usuarios = {
+    'taquitoadobado': {
+        'username': 'TaquitoAdobado',
+        'nombre': 'Taquito',
+        'apellido': 'Adobado',
+        'edad': 31,
+        'sexo': 'Masculino' },
+    'usuarioflask': {
+        'username': 'UsuarioFlask',
+        'nombre': 'Alex',
+        'apellido': 'Mundo',
+        'edad': 32,
+        'sexo': 'Masculino'},
+    'programadorpython': {
+        'username': 'ProgramadorPython',
+        'nombre': 'Carla',
+        'apellido': 'Juaneza',
+        'edad': 32,
+        'sexo': 'Femenino'}
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
