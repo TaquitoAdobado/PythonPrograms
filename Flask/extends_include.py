c412c4cb4061003de3ba051010c5a1988c729580
Daniel Teran Guerra <terandaniel94@gmail.com>
2026-02-05 01:02:43 -0600

    Se añade archivo extends_include.py en el cual se renderiza un html llamado hijo.html con el fin de explicar y entender el uso de los comandos extends e include. Se añade carpeta /extends_include la cual contiene los archivos: padre.html  -  Archivo html que funciona como estructura padre la cual se heredara a otro html mediante el comando extends. hijo.html  -  Archivo html que hereda la estructura de padre.html y sustituye bloques de codigo nombrados en padre.html footer.html  -  Archivo html con contenido que sera exportado a cualquier otro html que use el comando include.

---
+++ Flask/extends_include.py
@@ -0,0 +1,21 @@
+"""
+Este archivo se relaciona con los templates:
+- padre.html
+- hijo.html
+- footer.html
+
+Todos los templates se almacenaron en la carpeta templates/extends_input/ por temas de organizacion personal
+por lo cual se especifica como parametro la instancia de Flask llamada app
+
+En este ejemplo se renderiza el template hijo.html que extiende de padre.html, o sea, hereda su estructura y contenido reemplazando bloques definidos.
+Para mas informacion revisar los comentarios en los archivos de los templates relacionados listados arriba.
+"""
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="templates/extends_input/")
+
+@app.route('/')
+def home():
+    return render_template("hijo.html")
+
+if __name__=="__main__":
+    app.run(debug=True, port=5000, host="0.0.0.0")
